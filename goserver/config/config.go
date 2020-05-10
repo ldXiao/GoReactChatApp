b675677a10c0e85872ee782e15c8717d675755c6
ldXiao <xld704@gmail.com>
2020-05-10 14:01:07 -0400

    config

---
+++ goserver/config/config.go
@@ -0,0 +1,15 @@
+package config
+
+// DB connection string
+const ConnectionString = "mongodb://localhost:27017"
+
+// // Database Name
+const DbName = "test"
+
+// Collection name
+const CollNameUsers = "users"
+
+const CollNameChats = "chats"
+
+// hmacsecret
+const HmacSecretString = "secret"
