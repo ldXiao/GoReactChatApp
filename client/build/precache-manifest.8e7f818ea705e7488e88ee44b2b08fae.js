e0efdbf0ef021de14f70e9b718ba367867f034e4
ldXiao <xld704@gmail.com>
2020-05-10 13:50:55 -0400

    build

---
+++ client/build/precache-manifest.8e7f818ea705e7488e88ee44b2b08fae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5baf592f2a701fd4e9636e8781e8a43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cdfb245464aeabc7079",
+    "url": "/static/css/2.6547dd70.chunk.css"
+  },
+  {
+    "revision": "ec33a2a0c61729ce4741",
+    "url": "/static/css/main.a41aefa0.chunk.css"
+  },
+  {
+    "revision": "0cdfb245464aeabc7079",
+    "url": "/static/js/2.44c648aa.chunk.js"
+  },
+  {
+    "revision": "ec33a2a0c61729ce4741",
+    "url": "/static/js/main.864b0bd1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
